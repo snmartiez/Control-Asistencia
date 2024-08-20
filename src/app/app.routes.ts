8807b79361b224788cc564bf5fb493a45fe37c5b
Silfred Martinez <silfred.martinez@gmail.com>
2024-08-20 14:47:10 -0500

    Agregando de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfesoresComponent } from './pages/profesores/profesores.component';
+import { EstudiantesComponent } from './pages/estudiantes/estudiantes.component';
+import { ProgramasComponent } from './pages/programas/programas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',title:'Login',component:LoginComponent},
+    {path:'home',title:'Principal',component:HomeComponent,children:[
+        {path:'profesor',title:'Profesores',component:ProfesoresComponent},
+        {path:'estudiante',title:'Estudiantes',component:EstudiantesComponent},
+        {path:'programa',title:'Programas academicos',component:ProgramasComponent}
+    ]},
+    {path:'',redirectTo:'login',pathMatch:'full'}
+];
